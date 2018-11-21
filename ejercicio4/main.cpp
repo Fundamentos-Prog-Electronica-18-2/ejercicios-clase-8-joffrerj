3812399220afc65f37559606fa2ce8337894c90e
joffrerj <joffre.rj@outlook.com>
2018-11-21 09:29:34 -0500

    Ejercicio 4

---
+++ ejercicio4/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nombre, apellido;
+    int edad;
+    cout << "Ingrese su nombre: " << endl;
+    cin >> nombre;
+    cout << "Ingrese su apellido: " << endl;
+    cin >> apellido;
+    cout << "Ingrese su edad: " << endl;
+    cin >> edad;
+    if(edad<10){
+        cout << nombre << " " << apellido << endl
+                << "con: " << edad << " años" << endl
+                << "Pertenece al Basica.";
+    }else{
+        if((edad>=10) && (edad<14)){
+            cout << nombre << " "<< apellido << endl
+                << "con: " << edad << " años" << endl
+                << "Pertenece al Basica Superior";
+        }else{
+            if((edad>=14) && (edad<=17)){
+            cout << nombre << " "<< apellido << endl
+                << "con: " << edad << " años" << endl
+                << "Pertenece al Bachillerato Unificado";
+            }
+        }
+
+    }
+
+    return 0;
+}
